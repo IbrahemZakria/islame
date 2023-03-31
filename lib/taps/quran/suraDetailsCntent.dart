67a738e6459772d4d302e07a7197e0c29c89ea2c
IbrahemZakria <ibrahem.salama2000@gmail.com>
2023-04-01 01:42:55 +0200

    add hades widget

---
+++ lib/taps/quran/suraDetailsCntent.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class suraDEtailsContent extends StatefulWidget {
+  static const String id = 'suraDEtailsContent';
+
+  @override
+  State<suraDEtailsContent> createState() => _suraDEtailsContentState();
+}
+
+class _suraDEtailsContentState extends State<suraDEtailsContent> {
+  List<String> verses = [];
+
+  @override
+  Widget build(BuildContext context) {
+    var suraDetailsArgs =
+        ModalRoute.of(context)!.settings.arguments as suraNameModel;
+    if (verses.isEmpty) {
+      loadFile(suraDetailsArgs.index);
+    }
+
+    return Stack(children: [
+      Image.asset(
+        'assets/images/mainbg.png',
+        width: double.infinity,
+        height: double.infinity,
+        fit: BoxFit.fill,
+      ),
+      Scaffold(
+        appBar: AppBar(
+          title: Text(
+            '${suraDetailsArgs.name}',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return Text(
+              verses[index],
+              style: Theme.of(context).textTheme.headline2,
+              textAlign: TextAlign.center,
+            );
+          },
+          itemCount: verses.length,
+        ),
+      )
+    ]);
+  }
+
+  Future<void> loadFile(int index) async {
+    String content = await rootBundle.loadString(
+      'assets/fiels/${index + 1}.txt',
+    );
+    List<String> liens = content.split('\n');
+
+    verses = liens;
+    print('jkcs${verses[0]}');
+    setState(() {});
+  }
+}
+
+class suraNameModel {
+  String name;
+  int index;
+  suraNameModel(this.name, this.index);
+}
